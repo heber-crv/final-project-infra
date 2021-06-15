6b41bbc22fe2f115f27292ee3a4cf0a72ecd49d2
Heber.crv <heber.crv@outlook.com>
2021-06-14 21:11:45 -0500

    applied some best practices

--- variables.tf
+++ variables.tf
@@ -1,6 +1,26 @@
-variable "subscription_id" {}
-variable "client_id" {}
-variable "client_secret" {}
-variable "tenant_id" {}
-variable "ssh_key" {}
+variable "subscription_id" {
+  type=string
+  description="The subscription ID of the Azure account"
+}
+
+variable "client_id" {
+  type=string
+  description="The client ID of the Azure account"
+}
+
+variable "client_secret" {
+  type=string
+  description="The client secret of the Azure account"
+}
+
+variable "tenant_id" {
+  type=string
+  description="The tenant ID of the Azure account"
+}
+
+variable "ssh_key" {
+  type=string
+  description="The public SSH key to connect to the cluster"
+}
+
 #variable "blob_key" {}
